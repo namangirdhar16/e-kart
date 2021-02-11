e43fbc4f9e9151d51a4296056edda6d031541e2c
namangirdhar16 <girdharnaman1611@gmail.com>
2021-02-11 11:36:39 +0530

    e-kart setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import HomePage from './HomePage'
+export default class App extends React.Component{
+
+    render()
+    {
+        return (<div>
+            <HomePage />
+        </div>)
+    }
+}
